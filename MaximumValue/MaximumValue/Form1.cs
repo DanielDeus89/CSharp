bdcc3b02a661543f67e1c62db9d10bc13bdf5ae3
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-11 13:26:12 -0300

    First Commit

--- MaximumValue/MaximumValue/Form1.cs
+++ MaximumValue/MaximumValue/Form1.cs
@@ -16,5 +16,20 @@ namespace MaximumValue
         {
             InitializeComponent();
         }
+
+        double Maximum(double x, double y, double z)
+        {
+            return Math.Max(x, Math.Max(y, z));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double number1 = Double.Parse(firstNumberTextBox.Text);
+            double number2 = Double.Parse(secondNumberTextBox.Text);
+            double number3 = Double.Parse(thirdNumberTextBox.Text);
+
+            double maximum = Maximum(number1, number2, number3);
+
+            maximumLabel.Text = "Maximum is: " + maximum;
+        }
     }
 }
---
+++ RefOutTest/RefOutTest/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefOutTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void SquareRef(ref int x)
+        {
+            x = x * x;
+        }
+        void SquareOut(out int x)
+        {
+            x = 6;
+            x = x * x;
+        }
+        void Square(int x)
+        {
+            x = x * x;
+        }
+
+        private void showOutputButton_Click(object sender, EventArgs e)
+        {
+            int y = 5;
+            int z;
+
+            outputLabel.Text = "Original value of y: " + y + "\n";
+            outputLabel.Text += "Original value of z: unitialized \n\n";
+
+            SquareRef(ref y);
+            SquareOut(out z);
+
+            outputLabel.Text += "value of y after SquareRef " + y + "\n";
+            outputLabel.Text += "value of z after SquareOut " + z + "\n\n";
+
+            Square( y);
+            Square( z);
+
+
+            outputLabel.Text += "value of y after Square " + y + "\n";
+            outputLabel.Text += "value of z after Square " + z + "\n\n";
+
+
+        }
+    }
+}
