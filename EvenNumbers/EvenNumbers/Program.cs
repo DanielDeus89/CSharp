26976f391e5fc1d40a604c16c617859d9cc2cf20
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-12 19:09:03 -0300

    my sunday challenge

---
+++ EvenNumbers/EvenNumbers/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Desafio: Crie um programa que receba um número inteiro e imprima na tela todos os números pares de 0 até esse número (inclusive).
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            Console.Write("Enter an Integer Number: ");
+            number = Int32.Parse(Console.ReadLine());
+
+            for(int i = 1; i <= number; i++)
+            {
+                if(i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
