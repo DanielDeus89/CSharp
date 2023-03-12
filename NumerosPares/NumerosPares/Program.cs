4a6c7232a1f0e578a772a043804044353ca1a54c
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-12 20:27:38 -0300

    my sunday challenge

---
+++ NumerosPares/NumerosPares/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Desafio: Escreva um programa que solicite ao usuário para digitar um número inteiro positivo e imprima na tela todos os números pares entre 0 e o número digitado. 
+Se o número digitado for negativo ou zero, o programa deve exibir uma mensagem de erro e solicitar um novo número.
+*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumerosPares
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            while (true)
+            {
+                Console.Write("Insira um Numero inteiro positivo: ");
+                number = Int32.Parse(Console.ReadLine());
+
+                for (int i = 2; i <= number; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(i);
+                    }
+                }break;
+            }
+            Console.ReadKey();
+        }
+    }
+}
