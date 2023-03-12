d1f7c992d7dc24152404749c5520e80415efcd54
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-12 17:33:23 -0300

    my sunday challenge

---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,56 @@
+﻿/*
+Desafio: Escreva um programa que imprima todos os números primos entre 1 e 100 na tela.
+
+Requisitos:
+
+O programa deve imprimir apenas números primos.
+O programa deve imprimir os números na ordem em que são encontrados.
+Os números devem ser separados por espaços.
+O programa deve imprimir um número por linha.
+Exemplo de saída:
+
+2
+3
+5
+7
+11
+...
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2; i <= 100; i++)
+            {
+
+                bool isPrime = true;
+
+                for(int j = 2; j < i; j++)
+                {
+                    if (i % j == 0)
+                    {
+
+                        isPrime = false;
+                        break;
+                    }
+                }
+
+                if (isPrime)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+Nome do arquivo: ReverseString.cs
+
+Desafio: Escreva um programa que solicite ao usuário que digite uma string e, em seguida, imprima a string invertida na tela. Por exemplo, se o usuário digitar "hello", o programa deve imprimir "olleh".
+
+Requisitos:
+
+O programa deve solicitar ao usuário que digite uma string.
+O programa deve imprimir a string invertida na tela.
+O programa deve lidar com strings de comprimento variável.
+
+Exemplo de saída:
+
+Digite uma string: programming
+A string invertida é: gnimmargorp
+*/
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a String: ");
+            String input = Console.ReadLine();
+
+            char[] charArray = input.ToCharArray();
+
+            Array.Reverse(charArray);
+
+            String reversedString = string.Join("", charArray);
+
+            Console.WriteLine("The Inverted String is: {0}", reversedString);
+            Console.ReadKey();
+
+        }
+    }
+}
