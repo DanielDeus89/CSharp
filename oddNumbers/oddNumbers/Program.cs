5195747202181c54f8c676b504f51219ed8d632c
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-12 19:46:44 -0300

    my sunday challenge

---
+++ oddNumbers/oddNumbers/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Escreva um programa que imprima todos os números ímpares entre 1 e 100, exceto os múltiplos de 3. 
+Use uma estrutura de repetição e as operações lógicas que você aprendeu até agora. Boa sorte!
+
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 0; i <= 100; i++)
+            {
+                if((i % 2 != 0) && (i % 3 != 0))
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
