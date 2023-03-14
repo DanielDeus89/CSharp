7ce80ba209738fa1efaf7f4edf4a65698d9c5db0
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-13 21:01:04 -0300

    monday challenge

---
+++ CalculaAreaTriangulo/CalculaAreaTriangulo/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace CalculaAreaTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.btnCalcularArea = new System.Windows.Forms.Button();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lado A:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lado B:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Lado C:";
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(60, 21);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoA.TabIndex = 3;
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(60, 47);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoB.TabIndex = 4;
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(60, 73);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoC.TabIndex = 5;
+            // 
+            // btnCalcularArea
+            // 
+            this.btnCalcularArea.Location = new System.Drawing.Point(12, 107);
+            this.btnCalcularArea.Name = "btnCalcularArea";
+            this.btnCalcularArea.Size = new System.Drawing.Size(103, 23);
+            this.btnCalcularArea.TabIndex = 6;
+            this.btnCalcularArea.Text = "Calcular Area";
+            this.btnCalcularArea.UseVisualStyleBackColor = true;
+            this.btnCalcularArea.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(121, 107);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(44, 13);
+            this.lblResult.TabIndex = 7;
+            this.lblResult.Text = "Lado C:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 142);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.btnCalcularArea);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Button btnCalcularArea;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ CalculaAreaTriangulo/CalculaAreaTriangulo/Form1.cs
@@ -0,0 +1,43 @@
+﻿/*
+Desafio: Calcular a área de um triângulo a partir das medidas de seus lados.
+
+btnCalcularArea
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculaAreaTriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double Area(double ladoA, double ladoB, double ladoC)
+        {
+            double s = (ladoA + ladoB + ladoC) / 2;
+            return Math.Sqrt(s * (s - ladoA) * (s - ladoB) * (s - ladoC));
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double numberA = Double.Parse(txtLadoA.Text);
+            double numberB = Double.Parse(txtLadoB.Text);
+            double numberC = Double.Parse(txtLadoC.Text);
+
+            double resultado = Area(numberA, numberB, numberC);
+
+            lblResult.Text = "Area do Triangulo: " + resultado.ToString("F2");
+        }
+    }
+}
