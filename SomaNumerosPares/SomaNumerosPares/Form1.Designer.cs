78092509e8cbcd74e13a0d04d1c373938f5dd12d
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-14 19:59:53 -0300

    my tuesday challenge

---
+++ SomaNumerosPares/SomaNumerosPares/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SomaNumerosPares
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnAdicionar = new System.Windows.Forms.Button();
+            this.txtNumeros = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(147, 12);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(129, 73);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular Numeros da Lista";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(147, 98);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(87, 13);
+            this.lblResultado.TabIndex = 1;
+            this.lblResultado.Text = "Soma dos Pares:";
+            // 
+            // btnAdicionar
+            // 
+            this.btnAdicionar.Location = new System.Drawing.Point(12, 12);
+            this.btnAdicionar.Name = "btnAdicionar";
+            this.btnAdicionar.Size = new System.Drawing.Size(129, 73);
+            this.btnAdicionar.TabIndex = 2;
+            this.btnAdicionar.Text = "Adicionar Números a Lista";
+            this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
+            // 
+            // txtNumeros
+            // 
+            this.txtNumeros.Location = new System.Drawing.Point(12, 91);
+            this.txtNumeros.Name = "txtNumeros";
+            this.txtNumeros.Size = new System.Drawing.Size(129, 20);
+            this.txtNumeros.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(290, 134);
+            this.Controls.Add(this.txtNumeros);
+            this.Controls.Add(this.btnAdicionar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnAdicionar;
+        private System.Windows.Forms.TextBox txtNumeros;
+    }
+}
+
---
+++ SomaNumerosPares/SomaNumerosPares/Form1.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Desafio: Criar um programa que receba uma lista de números inteiros e 
+ * retorne a soma de todos os números pares da lista.
+ * */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SomaNumerosPares
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<int> numeros = new List<int>();
+
+        private void btnAdicionar_Click(object sender, EventArgs e)
+        {
+            string entrada = txtNumeros.Text.Trim();
+            string[] numerosString = entrada.Split(' ');
+
+            foreach(string numeroString in numerosString)
+            {
+                int numero;
+                if(int.TryParse(numeroString, out numero))
+                {
+                    numeros.Add(numero);
+                }
+            }
+            txtNumeros.Text = " ";
+            txtNumeros.Focus();
+        }
+
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int soma = 0;
+            foreach (int numero in numeros)
+            {
+                if (numero % 2 == 0)
+                {
+                    soma += numero;
+                    lblResultado.Text = "Soma dos Pares: " + soma;
+                }
+
+            }
+
+        }
+    }
+}
