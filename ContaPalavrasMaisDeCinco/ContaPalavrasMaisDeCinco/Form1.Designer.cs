8c715e309790b44a1c0703df10f1d4048e462c37
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-14 21:01:41 -0300

    my tuesday challenge

--- ContaPalavrasMaisDeCinco/ContaPalavrasMaisDeCinco/Form1.Designer.cs
+++ ContaPalavrasMaisDeCinco/ContaPalavrasMaisDeCinco/Form1.Designer.cs
@@ -28,13 +28,70 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblAdicionar = new System.Windows.Forms.Button();
+            this.txtFrase = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblAdicionar
+            // 
+            this.lblAdicionar.Location = new System.Drawing.Point(13, 13);
+            this.lblAdicionar.Name = "lblAdicionar";
+            this.lblAdicionar.Size = new System.Drawing.Size(154, 23);
+            this.lblAdicionar.TabIndex = 0;
+            this.lblAdicionar.Text = "Adicionar";
+            this.lblAdicionar.UseVisualStyleBackColor = true;
+            this.lblAdicionar.Click += new System.EventHandler(this.lblAdicionar_Click);
+            // 
+            // txtFrase
+            // 
+            this.txtFrase.Location = new System.Drawing.Point(13, 43);
+            this.txtFrase.Name = "txtFrase";
+            this.txtFrase.Size = new System.Drawing.Size(154, 20);
+            this.txtFrase.TabIndex = 1;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(173, 50);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(247, 13);
+            this.lblResultado.TabIndex = 2;
+            this.lblResultado.Text = "Quantidade de palavras com mais de 5 caracteres:";
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(173, 13);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(154, 23);
+            this.btnVerificar.TabIndex = 3;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.btnVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(455, 102);
+            this.Controls.Add(this.btnVerificar);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtFrase);
+            this.Controls.Add(this.lblAdicionar);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button lblAdicionar;
+        private System.Windows.Forms.TextBox txtFrase;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnVerificar;
     }
 }
 
--- ContaPalavrasMaisDeCinco/ContaPalavrasMaisDeCinco/Form1.cs
+++ ContaPalavrasMaisDeCinco/ContaPalavrasMaisDeCinco/Form1.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿/*
+Desafio: 
+criar um programa que receba uma lista de palavras e retorne a quantidade de palavras que possuem mais de 5 caracteres. 
+Nome sugerido para o arquivo: ContaPalavrasMaisDeCinco.cs.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +21,40 @@ namespace ContaPalavrasMaisDeCinco
         public Form1()
         {
             InitializeComponent();
+        }
+
+        List<string> palavras = new List<string>();
+
+        private void lblAdicionar_Click(object sender, EventArgs e)
+        {
+            string entrada = txtFrase.Text;
+            string[] fraseString = entrada.Split(' ');
+
+            palavras.Clear();
+
+            foreach(string palavra in fraseString)
+            {
+                palavras.Add(palavra);
+            }
+
+            txtFrase.Text = "";
+            
+        }
+
+        private void btnVerificar_Click(object sender, EventArgs e)
+        {
+            int count = 0;
+
+            foreach(string palavra in palavras)
+            {
+                if (palavra.Length > 5)
+                {
+                    count++;
+                }
+
+            }
+
+            lblResultado.Text = "Quantidade de palavras com mais de 5 caracteres: " + count;
         }
     }
 }
