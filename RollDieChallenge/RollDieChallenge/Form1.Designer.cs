dfef832a6b305f1f51cebb4e67bcbf8b12ed3da6
DanielDeus <73004139+DanielDeus89@users.noreply.github.com>
2023-03-16 20:11:08 -0300

    my Thursday challenge

---
+++ RollDieChallenge/RollDieChallenge/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace RollDieChallenge
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRollDie = new System.Windows.Forms.Button();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.txtDados = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtFaces = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnRollDie
+            // 
+            this.btnRollDie.Location = new System.Drawing.Point(13, 36);
+            this.btnRollDie.Name = "btnRollDie";
+            this.btnRollDie.Size = new System.Drawing.Size(183, 23);
+            this.btnRollDie.TabIndex = 0;
+            this.btnRollDie.Text = "Lançar Dados";
+            this.btnRollDie.UseVisualStyleBackColor = true;
+            this.btnRollDie.Click += new System.EventHandler(this.btnRollDie_Click);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.AutoSize = true;
+            this.lblOutput.Location = new System.Drawing.Point(10, 62);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(0, 13);
+            this.lblOutput.TabIndex = 1;
+            // 
+            // txtDados
+            // 
+            this.txtDados.Location = new System.Drawing.Point(202, 36);
+            this.txtDados.Name = "txtDados";
+            this.txtDados.Size = new System.Drawing.Size(151, 20);
+            this.txtDados.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(199, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Infome a quantidade de dados.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(359, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Infome a quantidade de faces.";
+            // 
+            // txtFaces
+            // 
+            this.txtFaces.Location = new System.Drawing.Point(362, 36);
+            this.txtFaces.Name = "txtFaces";
+            this.txtFaces.Size = new System.Drawing.Size(151, 20);
+            this.txtFaces.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(527, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFaces);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtDados);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.btnRollDie);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRollDie;
+        private System.Windows.Forms.Label lblOutput;
+        private System.Windows.Forms.TextBox txtDados;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtFaces;
+    }
+}
+
---
+++ RollDieChallenge/RollDieChallenge/Form1.cs
@@ -0,0 +1,73 @@
+﻿/* 
+Desafio:
+Crie um programa que simule um jogo de dados.
+O programa deve solicitar ao usuário quantos dados serão lançados e quantas faces cada dado terá. 
+Em seguida, deve exibir na tela o resultado de cada dado lançado e a soma total dos valores obtidos.
+
+Exemplo de saída:
+
+Quantos dados serão lançados? 3
+Quantas faces cada dado terá? 6
+
+Resultado dos dados lançados:
+Dado 1: 3
+Dado 2: 5
+Dado 3: 1
+
+Soma total: 9
+
+Dica: Utilize um loop para lançar cada dado e somar os valores obtidos. 
+Utilize o método Next da classe Random para gerar valores aleatórios para os dados.
+ * */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RollDieChallenge
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnRollDie_Click(object sender, EventArgs e)
+        {
+            Random randomInteger = new Random();
+
+            lblOutput.Text = "";
+
+            int dados = int.Parse(txtDados.Text);
+            int faces = int.Parse(txtFaces.Text);
+
+            int face = 0;
+            int soma = 0;
+
+            if (dados == 0)
+            {
+                lblOutput.Text = "valore in";
+            }
+
+            for (int i = 1; i <= dados; i++)
+            {
+               
+                face = randomInteger.Next(1, faces+1);                    
+                
+                soma += face;
+
+                lblOutput.Text += "Dado " + i + ":" + face + "\n";
+            }
+            lblOutput.Text += "\n\nSoma: " + soma.ToString();
+        }
+    }
+}
